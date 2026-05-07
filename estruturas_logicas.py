649859847dd0b3639357fb456427018024b17b1f
NuelSant <nuel.sant.07@gmail.com>
2026-05-06 22:17:56 -0300

    operadores_logicos

--- estruturas_logicas.py
+++ estruturas_logicas.py
@@ -1,1 +1,18 @@
-print("Olá Mundo!")
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+cnh = input("Tem CNH? (sim/não)")
+
+if idade >= 18 and cnh == "sim":
+    print("você pode dirigir👌")
+else:
+    print("você não pode dirigir")
+
+#operador or
+if idade <= 18 or idade >= 60:
+    estudante = input("vocé é estudante?(sim/não) ")
+    if estudante == "sim" or idade <= 60:
+        print("você ganhou um desconto para a sua CNH!")
+    else:
+        print("Vaza daqui")
+else: 
+    print("você não pode dirigir!")
